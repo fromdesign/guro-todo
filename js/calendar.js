288a47997648c5a55d62ed75b501508d5e2a2039
taebum <htb020@gmail.com>
2019-01-10 21:50:40 +0900

    'day2_tae'

--- js/calendar.js
+++ js/calendar.js
@@ -1,63 +1,99 @@
 var today = new Date();
 var week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
-var month = new Array('January','February','March','April','May','June','July','August','September','October','November','December',);
+var month = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 var year = today.getFullYear();
+var today_year = today.getYear();
 var today_day = today.getDay();
+var today_date = today.getDate();
 var month_day = today.getMonth();
 var todayLabel = week[today_day];
 var monthLabel = month[month_day];
+var todayMonth = new Date(year, month_day, 1).getDay(); //이번달 1일의 시작요일
+var lastday = new Date(year, month_day+1, 0); //다음달 1일의 전일 이번달 마지막 날의 시간
+var lastDate = new Date(lastday).getDate(); //이번달 마지막 날의 날짜 ex 31 30 
+var head = year + '-' + monthLabel;
 
-var head = year + monthLabel;
+
+function prevCalendar() {//이전 달
+         $('td').text('');
+         today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+         year = today.getFullYear();
+         month_day = today.getMonth();
+         todayMonth = new Date(year, month_day, 1).getDay();
+         lastDate = new Date(lastday).getDate();
+         monthLabel = month[month_day];
+         if(month_day == '-1'){
+             month_day = 11;
+             todayMonth = new Date(year, month_day, 1).getDay();
+             lastDate = new Date(lastday).getDate();
+             monthLabel = month[month_day];
+             head = year + '-' + monthLabel;
+             $('h1').text(head);
+             setCalendar ()
+         }else {
+             todayMonth = new Date(year, month_day, 1).getDay();
+             lastDate = new Date(lastday).getDate();
+             monthLabel = month[month_day];
+             head = year + '-' + monthLabel;
+             $('h1').text(head);
+             setCalendar ()
+         }
+        
+        }
+
+function nextCalendar() {//다음 달
+         $('td').text('');
+         today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+         year = today.getFullYear();
+         month_day = today.getMonth();
+         todayMonth = new Date(year, month_day, 1).getDay();
+         lastDate = new Date(lastday).getDate();
+         monthLabel = month[month_day];
+         if(month_day == '13'){
+             month_day = 0;
+             todayMonth = new Date(year, month_day, 1).getDay();
+             lastDate = new Date(lastday).getDate();
+             monthLabel = month[month_day];
+             head = year + '-' + monthLabel;
+             $('h1').text(head);
+             setCalendar ()
+         }else {
+             todayMonth = new Date(year, month_day, 1).getDay();
+             lastDate = new Date(lastday).getDate();
+             monthLabel = month[month_day];
+             head = year + '-' + monthLabel;
+             $('h1').text(head);
+             setCalendar ()
+         }
+        }
+
+function setCalendar () {
+    var x = todayMonth;
+    for (var i = 1; i <= lastDate; i++  ) {
+        $('td').eq(x).text(i);
+        x = x+1;
+    }
+}
+
+setCalendar();
+
 $('h1').text(head);
 
-$('#btn_today').click( function(){
-    $('#testDate').val(formatDate(today));
+$('td').eq(today_date+1).addClass('on');
+
+$('td').click(function(){
+    $('td').removeClass('on');
+    $(this).addClass('on');
+})
+
+
+
+$('#prev').click( function(){
+    prevCalendar()
 });
 
-$('#btn_yesterday').click( function(){
-    $('#testDate').val(yesterdayDate(today));
+$('#next').click( function(){
+    nextCalendar()
 });
 
-$('#btn_tomorrow').click( function(){
-    $('#testDate').val(tomorrowDate(today));
-});
 
-function formatDate(date) {
-    var d = new Date(date),
-//        month = '' + (d.getMonth() + 1),
-        
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-var todayLabel = week[today_day];
-var monthLabel = month[month_day];
- //   if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, monthLabel, day, todayLabel].join('-');
-}
-
-function yesterdayDate(date) {
-    var d = new Date(date),
-//        month = '' + (d.getMonth() + 1),
-        day = '' + (d.getDate() - 1),
-        year = d.getFullYear();
-var todayLabel = week[today_day - 1];
-var monthLabel = month[month_day];
- //   if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, monthLabel, day, todayLabel].join('-');
-}
-
-function tomorrowDate(date) {
-    var d = new Date(date),
-//        month = '' + (d.getMonth() + 1),
-        day = '' + (d.getDate() + 1),
-        year = d.getFullYear();
-var todayLabel = week[today_day + 1];
-var monthLabel = month[month_day];
- //   if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, monthLabel, day, todayLabel].join('-');
-}
